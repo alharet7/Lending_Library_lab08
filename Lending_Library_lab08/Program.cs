3b4f4357f969da0bedbe16384fcf002ba324a297
AlHareth alhyari <hdhareth@gmail.com>
2023-07-09 06:18:01 +0300

    Add project files.

---
+++ Lending_Library_lab08/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lending_Library_lab08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
